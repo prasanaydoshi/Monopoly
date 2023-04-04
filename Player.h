6020706c3a03f7ebad0180bdf027e491f27ed288
Talha Hussain <t25hussain@uwaterloo.ca>
2023-04-04 12:40:39 -0400

    added Player.h

---
+++ Player.h
@@ -0,0 +1,19 @@
+#ifndef PLAYER
+#define PLAYER
+
+#include <vector>
+#include <string>
+#include "Property.h"
+
+class Player {
+	int posn;
+	int balance;
+	std::vector<Property> owned;
+
+	public:
+	void changeposn();
+	int get_balance();
+	std::string get_properties();
+};
+
+#endif
