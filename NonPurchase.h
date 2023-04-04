18e813968e4f0ab2b7f9233b7b757577ddb216af
Talha Hussain <t25hussain@uwaterloo.ca>
2023-04-04 12:39:57 -0400

    added NonPurchase.h

---
+++ NonPurchase.h
@@ -0,0 +1,16 @@
+#ifndef NONPURCHASE
+#define NONPURCHASE
+
+class NonPurchase {
+public:
+	void TuitionPay();
+	void CoopFee();
+	void OSAPcol();
+	void TimsJail();
+	void GoToJail();
+	void SLC();
+	void NH();
+	void Nesting();
+}
+
+#endif
