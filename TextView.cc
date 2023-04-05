0599fc460faa6cf3584bcac7c8bc00afdc598bb4
Talha <t25hussa@uwaterloo.ca>
2023-04-05 17:04:12 -0400

    basic TextView made

---
+++ TextView.cc
@@ -0,0 +1,22 @@
+#include "TextView.h"
+
+ostream& TextView::PrintBoard() {
+};
+
+ostream& TextView::commands() {
+};
+
+ostream& TextView::DisplayNames() {
+};
+
+ostream& TextView::ShowInfo() {
+};
+
+ostream& TextView::prog_messages() {
+};
+
+ostream& TextView::menu() {
+};
+
+ostream& TextView::MakeAction() {
+};
---
+++ TextView.h
@@ -0,0 +1,14 @@
+#include "Data.h"
+#include <iostream>
+
+class Textview {
+public:
+	ostream& PrintBoard();
+	ostream& commands();
+	ostream& DisplayNames();
+	ostream& ShowInfo();
+	ostream& prog_messages();
+	ostream& menu();
+	ostream& MakeAction();
+};
+
