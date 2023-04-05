6d68f83ebab09e3fa58fb2c4f7deda06de0b4924
Talha Hussain <t25hussain@uwaterloo.ca>
2023-04-05 18:38:56 -0400

    Updated Player.h

--- Player.h
+++ Player.h
@@ -9,8 +9,7 @@ class Player {
 	int posn;
 	int balance;
 	std::vector<Property> owned;
-	std::vector<int> rent;
-//doesnt this require more fields??
+	
 	public:
 	void changeposn();
 	int get_balance();
