7117d2ebadf7ed349c623aa3a18d6b02cf1cfe15
Talha <t25hussa@uwaterloo.ca>
2023-04-05 16:44:40 -0400

    added Controller.cc file

---
+++ Controller.cc
@@ -0,0 +1,64 @@
+#include "Controller.h"
+#include "Data.h"
+#include "command.h"
+
+void Controller::imp_buy() {
+
+};
+
+void Controller::imp_sell() {
+
+};
+
+void Controller::next_turn() {
+	
+};
+
+void Controller::roll_func() {
+
+};
+
+void Controller::roll() {
+
+};
+
+void Controller::test_roll() {
+
+};
+
+void Controller::trade() {
+
+};
+
+void Controller::trade_offer() {
+
+};
+
+void Controller::mortgage() {
+
+};
+
+void Controller::unmortgage() {
+
+};
+
+void Controller::owned_properties() {
+
+};
+
+void Controller::all() {
+
+};
+
+void Controller::decl_bankrupt() {
+
+};
+
+void Controller::load_game(string filein) {
+
+};
+
+void Controller::save(string fileout) {
+	
+};
+
