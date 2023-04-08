2f2fbfb4a485e49fe96f63f9ea59e700279cbfd9
Talha <t25hussa@uwaterloo.ca>
2023-04-08 14:46:47 -0400

    added implementation for some methods in TextView and made some other changes

--- TextView.cc
+++ TextView.cc
@@ -1,15 +1,52 @@
 #include "TextView.h"
 
 ostream& TextView::PrintBoard() {
+
 };
 
-ostream& TextView::commands() {
+// function to display commands to player:
+ostream& TextView::commands(ostream& os) {
+	os << "\nThe possible commands a Player can make include:\n";
+	os << "	roll\n";
+	os << "		roll : rolls two dice and moves player by their sum\n\n";
+	os << "	next\n";
+	os << "		next : when your turn is up, switches to next player in line\n\n";
+	os << "	trade <name> <give> <receive>\n";
+	os << "		trade : current player can request a trade from another player,";
+	os << "must mention their name, what they are offering and what they want in return\n\n";
+	os << "	improve <property> buy/sell\n";
+	os << "		improve: current player can attempt to buy or sell an improvement 
+	os << "for the mentioned property\n\n";
+	os << "	mortgage <property>\n";
+	os << "		mortgage : current player can attempt to mortgage the mentioned property\n\n";
+	os << "	unmortgage <property>\n";
+	os << "		unmortgage : current player can attempt to unmortgage the mentioned 
+	os << "property\n\n";
+	os << "	bankrupt\n";
+	os << "		bankruptcy : player declares bankruptcy, only allowed when player owes more then 
+	os << "their balance\n\n";
+	os << "	assets\n";
+	os << "		assets : displays assets of current player\n\n";
+	os << "	all\n";
+	os << "		all : displays assets of all players\n\n";
+	os << "	save <filename>\n";
+	os << "		save : saves the current state of game in a file with the mentioned 
+	os << "filename\n\n";
+	return os;
 };
 
-ostream& TextView::DisplayNames() {
+// function to display current playernames
+ostream& TextView::DisplayNames(ostream& os) {
+	int players = Game_data.get_noPlayers();
+	os << "Currently still in-game:\n";
+	for (int i = 0; i <= players; ++i) {
+		os << "Playerno. " << i << " : " << Game_data.get_namePlayer(i);
+	}
+	return os;
 };
 
 ostream& TextView::ShowInfo() {
+	
 };
 
 ostream& TextView::prog_messages() {
@@ -19,4 +56,20 @@ ostream& TextView::menu() {
 };
 
 ostream& TextView::MakeAction() {
+};
+
+// shows assets of current player
+std::ostream& cur_assets(std::ostream& os) {
+	os << Game_data.get_dataCurr();
+	return os;
+};
+
+//shows assets of all players
+std::ostream& all_assets(std::ostream& os) {
+	int players = Game_data.get_noPlayers();
+	os << "Assets of Currenlty Active players:\n";
+	for (int i = 0; i <= players; ++i) {
+		os << "Playerno. " << i << " : " << Game_data.get_dataPlayer(i);
+	}
+	return os;
 };
--- TextView.h
+++ TextView.h
@@ -3,12 +3,19 @@
 
 class TextView {
 public:
-	std::ostream& PrintBoard();
-	std::ostream& commands();
-	std::ostream& DisplayNames();
-	std::ostream& ShowInfo();
-	std::ostream& prog_messages();
-	std::ostream& menu();
-	std::ostream& MakeAction();
-;
+	// prints the current state of the board
+	std::ostream& PrintBoard(std::ostream&);
+	// shows the commands the player could make
+	std::ostream& commands(std::ostream&);
+	// shows the names of all the players
+	std::ostream& DisplayNames(std::ostream&);
+	// 
+	std::ostream& ShowInfo(std::ostream&);
+	//
+	std::ostream& prog_messages(std::ostream&);
+	// text-based menu screen with options
+	std::ostream& menu(std::ostream&);
+	// 
+	std::ostream& MakeAction(std::ostream&);
+};
 
