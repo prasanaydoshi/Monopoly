b67f8fc5c30df2a190c563ffc6d5b6bd48f5ed80
Talha Hussain <t25hussa@uwaterloo.ca>
2023-04-09 23:36:22 -0400

    juuust about finished Player

--- Player.cc
+++ Player.cc
@@ -2,23 +2,25 @@
 
 // constructor
 Player::Player(std::string name, char piece): name{name}, piece{piece},
-        posn{0}, balance{1500}, timsCups{0} { }
+        posn{0}, balance{1500}, timsCups{0}, inJail{false}, jailRolls{1} { }
 
 // accessors
-std::string Player::get_name() const { return name; }
-char Player::get_piece() const { return piece; }
-int Player::get_posn() const { return posn; }
-int Player::get_balance() const { return balance; }
-int Player::get_timsCups() const { return timsCups; }
-int Player::No_ownedProperties() const { return owned.size(); }
-int Player::get_TileNo(int i) const { return owned[i]; }
+std::string Player::getName() const { return name; }
+char Player::getPiece() const { return piece; }
+int Player::getPosn() const { return posn; }
+int Player::getBalance() const { return balance; }
+int Player::getTimsCups() const { return timsCups; }
+std::vector<int> Player::getProperties() const { return owned; }
+bool Player::isInJail() const { return inJail; }
+int Player::jailRollsCount() const { return jailRolls; }
+//int Player::No_ownedProperties() const { return owned.size(); }
+//int Player::get_TileNo(int i) const { return owned[i]; }
 
 // mutators
-void Player::JumpTo_posn(int a) { posn = a; 
-//will mostly be need to be used with jail
-
+void Player::jumpToPosn(int a) { posn = a; }
 }
-void Player::MovePosn_By(int a) {
+
+void Player::movePosnBy(int a) {
 	posn += a;
 	if (posn > 39) {
 		posn = posn % 39;
@@ -26,12 +28,27 @@ void Player::MovePosn_By(int a) {
 		//call go function and increment the player bal by 200
 }
 
-void Player::pay(int a) {
-	balance += a;
+void Player::subMoney(int a) { balance -= a; }
+
+void Player::addMoney(int a) { balance += a; }
+
+void Player::addPropery(int a) { owned.push_back(a); }
+
+void Player::setInJail() {
+	if (inJail) {
+		inJail = false;
+		jailRolls = 1;
+	} else {
+		inJail = true;
+	}
 }
 
-void Player::buy(int a) {
-	balance -= a;
+bool Player::setJailRolls() {
+	++jailRolls;
+	if (jailRolls >= 3) {
+		return true;
+	}
+	return false;
 }
 
 //other methods
@@ -45,7 +62,7 @@ void Player::nextposn() {
 
 //int calc_NetWorth() {}
 
-std::string Player::get_properties() {
-	// TODO: implement this method
-	return "";
-}
+//std::string Player::get_properties() {
+	// xxTODO: implement this method
+//	return "";
+//}
