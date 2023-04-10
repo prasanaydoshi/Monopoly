9ea979f74383edeafdc27dc9e026b121f0667089
Talha Hussain <t25hussa@uwaterloo.ca>
2023-04-10 00:09:04 -0400

    slight changes to data.h

--- Data.h
+++ Data.h
@@ -3,14 +3,15 @@
 
 #include <iostream>
 #include <vector>
-#include <map>
 #include <memory>
 #include <random>
 #include "Player.h"
-#include "BoardTiles.h"
+#include "board.h"
 
 class Data {
 	std::unique_ptr<Player> current;
+	int curPlayer;
+	bool rolled;
 	std::unique_ptr<Player> bank;
 	std::vector<std::unique_ptr<Player>> players;
 	std::vector<std::unique_ptr<BoardTiles>> board;
@@ -38,17 +39,25 @@ public:
 	void getPlayerNames();				//? prints out all names of players
 	
 	//nested accessors
-	std::string get_namePlayer(int i);
-	char get_piecePlayer(int i);
-	int get_posPlayer(int i);
-	int get_numPlayer(int i);
-	int get_BalPlayer(int i);
+	void printBlock();
+	bool playerInJail() const;
+	int ownsProperty(std::string property);
 
-	std::string get_TileName(i);		// must be implemented
-	std::string get_TileOwner(i);		//
-	int get_TileImpLevel(i);		//
+	std::string getNamePlayer(int i);
+	char getPiecePlayer(int i);
+	int getPosPlayer(int i);
+	int getNumPlayer(int i);
+	int getBalPlayer(int i);
+
+	std::string getTileName(i);		// must be implemented
+	std::string getTileOwner(i);		//
+	int getTileImpLevel(i);		//
 
 	//additional methods
+	void checkTile(int i);
+	void improve(std::string property);
+	void unimprove(std::string property);
+
 	int get_NetWorth();
 	std::string owned_assets(int i);
 	void save(std::ostream& os) const;
